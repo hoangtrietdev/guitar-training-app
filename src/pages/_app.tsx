1a61c051a0f4df1e082eb52f0435bc689ea3cddd
hoangtrietdev <hoangtrietdev@gmail.com>
2026-05-12 14:57:33 +0200

    chore: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </Head>
+      <Analytics />
       <Component {...pageProps} />
     </>
   );
